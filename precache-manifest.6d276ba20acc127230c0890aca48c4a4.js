7e2eb522590feb2cb9bbc09eb70880b7f60db2c3
ihsan <ihsanrawi@gmail.com>
2019-03-27 23:16:08 +0800

    Updates

---
+++ precache-manifest.6d276ba20acc127230c0890aca48c4a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8f01488834ea58cd322",
+    "url": "/Neptune/static/css/main.9b27fb6d.chunk.css"
+  },
+  {
+    "revision": "e8f01488834ea58cd322",
+    "url": "/Neptune/static/js/main.e8f01488.chunk.js"
+  },
+  {
+    "revision": "c68fe54813a83d1bb460",
+    "url": "/Neptune/static/js/runtime~main.c68fe548.js"
+  },
+  {
+    "revision": "b4270114be35911eaca9",
+    "url": "/Neptune/static/js/2.b4270114.chunk.js"
+  },
+  {
+    "revision": "d39a97399b64571dcc49da6905057204",
+    "url": "/Neptune/index.html"
+  }
+];
