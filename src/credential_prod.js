0b64124c375d81bcbb3f4c83952e1cf29844e11c
ihsan <ihsanrawi@gmail.com>
2019-03-30 00:14:19 +0800

    Uses .env to store clientID instead from a .js file

--- src/credential_prod.js
+++
@@ -1,7 +0,0 @@
-import pkg from '.././package.json'
-const browserUA = navigator.userAgent.toLocaleLowerCase();
-
-export default {
-  userAgent: `${browserUA}:${pkg.name}:v${pkg.version}`,
-  clientId: process.env.CLIENT_ID
-}
--- src/credentials.js
+++ src/credentials.js
@@ -1,5 +1,10 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require('./credential_prod')
-} else {
-  module.exports = require('./credential_dev')
+import pkg from '.././package.json'
+
+const browserUA = navigator.userAgent.toLocaleLowerCase();
+// clientId is retrieve from .env file
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
+export default {
+  userAgent: `${browserUA}:${pkg.name}:v${pkg.version}`,
+  clientId: clientId
 }
