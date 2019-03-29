736d1ec2e1d2a465b29169ed9e9fa29dca966502
Ihsan <ihsanrawi@gmail.com>
2019-03-29 20:06:22 +0800

    Remove dev credential from git

--- src/credential_dev.js
+++
@@ -1,7 +0,0 @@
-import pkg from '.././package.json'
-const browserUA = navigator.userAgent.toLocaleLowerCase();
-
-export default {
-  userAgent: `${browserUA}:${pkg.name}:v${pkg.version}`,
-  clientId: "KNLSx222D6ygiA"
-}
