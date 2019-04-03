b9a71b8e961e993cc0b93e9f8b8e6f76429563e1
ihsan <ihsanrawi@gmail.com>
2019-04-03 23:31:16 +0800

    uses postfeed functional class instead of postfeed container

---
+++ src/components/PostFeed/index.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import PropTypes from "prop-types";
+import './PostFeed.scss';
+
+import Loading from '../Loading'
+import Post from '../Post'
+
+function PostFeed({posts, loading, loadMorePosts, loadingMore}) {
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  function handleScroll() {
+    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+    loadMorePosts()
+  }
+
+  if(loading){
+    return <Loading type="fullscreen"/>
+  }
+
+  if(posts.length === 0) {
+    return <div>No post found. :C</div>
+  }
+  
+  return (
+    <div className="post-feed">
+      {posts.map(post => <Post key={post.id} post={post} />)}
+      {loadingMore && <Loading type="inline" />}
+    </div>
+  )
+}
+
+PostFeed.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  loading: PropTypes.bool.isRequired,
+  loadMorePosts: PropTypes.func.isRequired,
+  loadingMore: PropTypes.bool.isRequired,
+};
+
+export default PostFeed;
