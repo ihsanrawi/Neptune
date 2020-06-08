641b78d40ecbd091f4424f8a7a680ee069c08811
Ihsan <ihsanrawi@gmail.com>
2020-06-08 21:35:56 +0800

    Updates

---
+++ precache-manifest.77612ac76bc1f77189436648e45fa86c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec5f49e8d2ba65e568722016e402558",
+    "url": "/Neptune/index.html"
+  },
+  {
+    "revision": "9308ffc635c6f30602f3",
+    "url": "/Neptune/static/css/main.736ec359.chunk.css"
+  },
+  {
+    "revision": "73c750d0e31ab7904131",
+    "url": "/Neptune/static/js/2.3ac278b7.chunk.js"
+  },
+  {
+    "revision": "9afa15926ea855fdeac091a4c41a7d50",
+    "url": "/Neptune/static/js/2.3ac278b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9308ffc635c6f30602f3",
+    "url": "/Neptune/static/js/main.f727f9e7.chunk.js"
+  },
+  {
+    "revision": "505a7257bbb2ae046a6f",
+    "url": "/Neptune/static/js/runtime-main.5dd1b345.js"
+  }
+]);
